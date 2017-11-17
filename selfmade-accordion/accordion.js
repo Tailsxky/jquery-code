2488f43989ba8ec3a40e962f23cc78a189486225
tailsxky <xky900410@163.com>
2017-11-17 10:09:02 +0800

    jquery-learning

---
+++ selfmade-accordion/accordion.js
@@ -0,0 +1,28 @@
+    (function($) {
+      $.fn.accordion = function(options) {
+        return this.each(function() {
+          var dts = $(this).children('dt');
+          dts.click(onClick);
+          dts.each(reset);
+        });
+     
+        function onClick() {
+          $(this).siblings('dt').each(hide);
+          $(this).next().slideDown('fast');
+          return false;
+        }
+     
+        function hide() {
+          $(this).next().slideUp('fast');
+        }
+     
+        function reset() {
+          $(this).next().hide();
+        }
+      }
+    })(jQuery);
+
+$(function(){
+	//调用你的自定义插件
+	$('dl#my-accordion').accordion();
+});
